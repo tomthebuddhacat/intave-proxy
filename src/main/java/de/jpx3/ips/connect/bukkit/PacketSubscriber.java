e87505f5544df66b01e58835aaca063a27e241c0
tomthebuddhacat <bharteeshbrao@gmail.com>
2026-05-03 18:16:44 +0530

    Delete src/main directory

--- src/main/java/de/jpx3/ips/connect/bukkit/PacketSubscriber.java
+++
@@ -1,7 +0,0 @@
-package de.jpx3.ips.connect.bukkit;
-
-import com.velocitypowered.api.proxy.Player;
-
-public interface PacketSubscriber<P extends AbstractPacket> {
-  void handle(Player sender, P packet);
-}
